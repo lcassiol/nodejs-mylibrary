3ea4bbaea9389f8d8074657d727298a8cf76bff8
Cassio C <lccassiol@gmail.com>
2019-04-09 19:53:00 -0300

    Add Book model

---
+++ src/app/models/Book.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Book', BookSchema)
