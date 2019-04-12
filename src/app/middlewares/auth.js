435b8b9dc065e7aa6b8a484e9f2c0581dea6ee76
Cassio C <lccassiol@gmail.com>
2019-04-12 10:52:22 -0300

    Implements validation when user was deleted and try consume endpoints

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const authConfig = require('../../config/auth')
 const { promisify } = require('util')
+const User = require('../models/User')
 
 module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization
@@ -15,6 +16,15 @@ module.exports = async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret)
 
     req.userId = decoded.id
+
+    const user = await User.findOne({ _id: req.userId })
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: "You don't have power here anymore" })
+    }
+
     return next()
   } catch (error) {
     return res.status(401).json({ error: 'Token invalid' })
